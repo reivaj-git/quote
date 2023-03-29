13711a71d7dc518872f742dfa95d634f778b8144
Ricardo <javi26028@outlook.com>
2023-03-28 21:04:48 -0600

    Ajustando Marfin Tops y altura del card frase

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({heandleNewQuote}) => {
   return (
     <button
       onClick={heandleNewQuote}
-      className="mt-24 bg-white hover:bg-orange text-gray-600 hover:text-white font-semibold py-2 px-4  rounded shadow"
+      className="mt-20 bg-white hover:bg-orange text-gray-600 hover:text-white font-semibold py-2 px-4  rounded shadow"
     >
       Probar mi suerte
     </button>
